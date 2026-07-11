ece6ef69439ee834bb814055be73dba76df34c82
Alex Lee <arl7816@g.rit.edu>
2026-07-11 16:58:11 -0400

    Creates the PlayerController file

---
+++ Lights Out/Assets/Prefabs/Player/Scripts/Movement/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+Acts as the general manager for all communication between different aspects of the player movement.
+This also holds the internal state machine for the player movement.
+
+*/
+
+public class PlayerController : MonoBehaviour
+{
+    
+}
