2ba9c58041b54b83ec2b7b3242dec15e92c0c836
Alex Lee <arl7816@g.rit.edu>
2026-07-18 13:18:09 -0400

    fix this wack ass formatting

--- uhhh yeahhh/Assets/LogicScript.cs
+++ uhhh yeahhh/Assets/LogicScript.cs
@@ -6,27 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
- public int playerScore;
- public Text scoreText;
- public GameObject gameOverScreen;
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
  
 
-[ContextMenu("Increase Score")]
- public void addScore(int scoreToAdd)
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
     {
         playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
     }
 
-public void restartGame()
+    public void restartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-public void gameOver()
+    public void gameOver()
     {
-        gameOverScreen.SetActive(true);
-        
+        gameOverScreen.SetActive(true);  
     }
 
 
